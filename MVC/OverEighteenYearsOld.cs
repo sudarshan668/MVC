599bd162a7c6819655da6c8ff273f80030a4cb21
sudarshan sonawane <ssid668@hotmail.com>
2018-06-05 17:46:37 +0530

    working 18+ validation for birthday

---
+++ MVC/OverEighteenYearsOld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class OverEighteenYearsOld : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return value != null && value is DateTime && ((DateTime)value).AddYears(18).Date <= DateTime.Now.Date;
+        }
+    }
+}
